c676a895680be2747526281beb7fe2b5f968fc2a
Peter Zandbergen <peter.zandbergen@myhops.com>
2025-02-11 20:57:46 +0000

    Save work

--- exercises/manual-instrumentation-traces/initial/src/app.py
+++ exercises/manual-instrumentation-traces/initial/src/app.py
@@ -4,10 +4,17 @@ import time
 
 import requests
 from client import ChaosClient, FakerClient
-from flask import Flask, make_response
+from flask import Flask, make_response, request
+
+from trace_utils import create_tracer
+
+from opentelemetry import trace as trace_api
+from opentelemetry.semconv.trace import SpanAttributes
 
 # global variables
 app = Flask(__name__)
+tracer = create_tracer("app.py", "0.1")
+
 
 @app.route("/users", methods=["GET"])
 def get_user():
@@ -19,6 +26,7 @@ def get_user():
     return response
 
 
+@tracer.start_as_current_span("do_stuff")
 def do_stuff():
     time.sleep(0.1)
     url = "http://localhost:6000/"
@@ -27,7 +35,16 @@ def do_stuff():
 
 
 @app.route("/")
+@tracer.start_as_current_span("index")
 def index():
+    span = trace_api.get_current_span()
+    span.set_attributes(
+        {
+            SpanAttributes.HTTP_REQUEST_METHOD: request.method,
+            SpanAttributes.URL_PATH: request.path,
+            SpanAttributes.HTTP_RESPONSE_STATUS_CODE: 200,
+        }
+    )
     do_stuff()
     current_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
     return f"Hello, World! It's currently {current_time}"
