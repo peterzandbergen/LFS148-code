2bef0d2fd074e0bae67edc7336b8b9ddeddf8b4b
Peter Zandbergen <peter.zandbergen@myhops.com>
2025-02-11 21:49:06 +0000

    Make simple metrics

--- exercises/manual-instrumentation-metrics/initial/src/app.py
+++ exercises/manual-instrumentation-metrics/initial/src/app.py
@@ -4,10 +4,15 @@ import time
 
 import requests
 from client import ChaosClient, FakerClient
-from flask import Flask, make_response
+from flask import Flask, make_response, request
+
+# custom
+from metric_utils import create_meter, create_request_instruments
 
 # global variables
 app = Flask(__name__)
+meter = create_meter("app.py", "0.1")
+
 
 @app.route("/users", methods=["GET"])
 def get_user():
@@ -26,13 +31,15 @@ def do_stuff():
     return response
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    request_instruments["index_counter"].add(1, {"http.request.method": request.method})
     do_stuff()
     current_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
     return f"Hello, World! It's currently {current_time}"
 
 
 if __name__ == "__main__":
+    request_instruments = create_request_instruments(meter)
     db = ChaosClient(client=FakerClient())
     app.run(host="0.0.0.0", debug=True)
